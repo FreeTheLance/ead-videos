1ffca12b23bcb6f110f35db4693d7b5f977d6804
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2015-09-21 22:59:25 -0300

    Cleanup script.

--- js/script.js
+++ js/script.js
@@ -1,62 +1,22 @@
-var canvas = document.getElementById("main-canvas");
- 
-var context = canvas.getContext("2d");
- 
-var video = document.getElementById("main-video");
 
+jQuery(function () {
 
-var buttonObject =
- 
-{
- 
-  width: 0,
- 
-  height: 0,
- 
-  x: 0,
- 
-  y: 0,
- 
-  centerX: function()
- 
-  {
- 
-    return this.x + (this.width / 2);
- 
-  },
- 
-  centerY: function()
- 
-  {
- 
-    return this.y + (this.height / 2);
- 
-  },
- 
-  halfWidth: function()
- 
-  {
- 
-    return this.width / 2;
- 
-  },
- 
-  halfHeight: function()
- 
-  {
- 
-    return this.height / 2;
- 
-  }
- 
-};
+  var $video1 = $('#video1');
+  var $video1_1 = $('#video1_1');
+  var $video2 = $('#video2');
+  var $video2_1 = $('#video2_1');
+  var $video3 = $('#video3');
+  var $video4 = $('#video4');
+  var $video4_1 = $('#video4_1');
+  var $video5 = $('#video5');
+  var $video5_1 = $('#video5_1');
+  var $video6 = $('#video6');
+  var $video6_1 = $('#video6_1');
+  var $video7 = $('#video7');
+  var $video7_1 = $('#video7_1');
+  var $video8 = $('#video8');
+  var $video8_1 = $('#video8_1');
+  var $video9 = $('#video9');
+  var $encerramento = $('#encerramento');
 
-var button = Object.create(buttonObject);
- 
-button.x = 200;
- 
-button.y = 200;
-
-context.fillStyle = "#333";
- 
-context.fillRect(button.x,button.y,button.width,button.height);
+});
