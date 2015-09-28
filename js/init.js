0a262bd836e556f3fc5a4c28140d3a700846a7fe
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2015-09-27 22:37:52 -0300

    Play/pause video on click.

--- js/init.js
+++ js/init.js
@@ -2,14 +2,24 @@
  * Setup initial listeners.
  */
 
-// Empty hash for Gods sake!
-if (location.hash) {
-  location.hash = '';
-}
+(function () {
 
-$(document).on('keydown', function (e) {
-  if (e.keyCode == 32 && EADVideos.current) {
-    var api = EADVideos.current.api;
-    api[api.paused() ? 'play' : 'pause']();
+  var video;
+
+  // Empty hash for Gods sake!
+  if (location.hash) {
+    location.hash = '';
   }
-});
+
+  $(document).on('keydown', function (e) {
+    if (e.keyCode == 32) toggleCurrentVideoState();
+  });
+
+  $('video').on('click', toggleCurrentVideoState);
+
+  function toggleCurrentVideoState() {
+    if (video = EADVideos.current && EADVideos.current.api) {
+      video[video.paused() ? 'play' : 'pause']();
+    }
+  }
+})();
